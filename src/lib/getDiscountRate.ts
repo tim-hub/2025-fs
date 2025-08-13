a654fa9644732e5f65e58995aa55fd4992a9c498
tim-hub <tim-hub@users.noreply.github.com>
2025-08-13 18:39:37 +1200

    test more on get discount rate

--- src/lib/getDiscountRate.ts
+++ src/lib/getDiscountRate.ts
@@ -1,19 +1,19 @@
 
 export const getDiscountRate = (orderValue: number): number => {
 
-  if (orderValue > 50000) {
+  if (orderValue >= 50000) {
     return 0.15
   }
-  else if (orderValue > 10000) {
+  else if (orderValue >= 10000) {
     return 0.1
   }
-  else if (orderValue > 7000) {
+  else if (orderValue >= 7000) {
     return 0.07
   }
-  else if (orderValue > 5000) {
+  else if (orderValue >= 5000) {
     return 0.05
   }
-  else if (orderValue > 1000) {
+  else if (orderValue >= 1000) {
     return 0.03
   }
   else {
